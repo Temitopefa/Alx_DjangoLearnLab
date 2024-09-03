26c2b3e5b929e58749b5967ed251f1a7d36ef3d8
temitopefa <lasbohoo@gmail.com>
2024-09-03 23:02:51 +0100

    auth function

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import list_books, LogoutView, LoginView, admin_view, librarian_view, member_view
+from .views import list_books, LogoutView, LoginView, admin_view, librarian_view, member_view,add_book, edit_book, delete_book
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # URL for function-based view
@@ -11,4 +11,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', delete_book, name='delete_book'),
 ]
