8726bfbc6f0fb9823cf21f8d39c4465501cd8df9
temitopefa <lasbohoo@gmail.com>
2024-10-15 16:04:40 +0100

    feat:CRUD operation

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,8 +1,17 @@
 # api/urls.py
 
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, BookList
+
+# Create a router instance
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')  # Register the BookViewSet
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    # Route for the BookList view (ListAPIView)
+    path('books/', BookList.as_view(), name='book-list'),
+
+    # Include the router URLs for all CRUD operations
+    path('', include(router.urls)),
 ]
